8ed151849983ba309502779dec6bbc63b111affb
NTT1906 <nttispay@gmail.com>
2025-04-07 16:00:33 +0700

    REMOVE: DUMP FILE [IMPORTANT, PLEASE CHECK LATER MY FUTURE SELF]

--- dump/file.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-def process_file(input_path):
-    input_path = Path(input_path)
-    return input_path.with_name('output_' + input_path.stem[6:] + input_path.suffix)
-
-# Example usage
-process_file('path/to/input_abcd.txt')
