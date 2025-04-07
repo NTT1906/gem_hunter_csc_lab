e0468768d34e39bd1167116d513fc42ccc98cdbc
NTT1906 <nttispay@gmail.com>
2025-04-07 15:59:37 +0700

    remove old asset

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def run(file_name):
 	cnf = generate_cnf(grid)
 
 	print(cnf.clauses)
-	print(f"Clauses: {len(cnf.clauses)}")
+	print(f"[!] Clauses: {len(cnf.clauses)}")
 
 	scoped_cells = []
 
@@ -29,7 +29,7 @@ def run(file_name):
 						scoped_cells.append((r, c))
 
 	def test(solution: ISolution, solution_name: str):
-		print(f"Solution: {solution_name}")
+		print(f"[+] Solution: {solution_name}")
 		elapsed_time = -time.time()
 		result = solution.solve(grid, cnf)
 		if result is None:
@@ -38,27 +38,34 @@ def run(file_name):
 			model = result.model
 			rows = len(grid)
 			cols = len(grid[0])
+			output_grid = [row[:] for row in grid]
 			for trow in range(rows):
 				for tcol in range(cols):
-					if grid[trow][tcol] == 0:
-						if (trow, tcol) not in scoped_cells:
-							print('_', end=' ')
-							continue
-						m = trow * cols + tcol + 1
-						if m in model:
-							print("T", end=' ')
+					if grid[trow][tcol] == 0 and (trow, tcol) in scoped_cells:
+						var_index = trow * cols + tcol + 1
+						if var_index in model:
+							output_grid[trow][tcol] = 'T'
 						else:
-							print("G", end=' ')
-					else:
-						print(f"{grid[trow][tcol]}", end=' ')
-				print('')
+							output_grid[trow][tcol] = 'G'
+					elif grid[trow][tcol] == 0 and (trow, tcol) not in scoped_cells:
+						output_grid[trow][tcol] = '_'
+
+			for trow in output_grid:
+				print(' '.join(map(str, trow)))
+			print(f"Elapsed time: {result.elapsed_time:.4f}ms")
+			if solution_name == "Pysat":
+				output_file = get_output_name_from_input_file(file_name)
+				with open(output_file, 'w') as file:
+					print(f"Solution being saved to {output_file}")
+					for trow in output_grid:
+						file.write(', '.join(map(str, trow)) + '\n')
 		elapsed_time += time.time()
-		print(f"Real time: {elapsed_time * 1000:.3f}ms")
+		print(f"Run time: {elapsed_time * 1000:.4f}ms")
 
 	solutions = {
-		"pysat": PysatSolution(),
-		"backtrack": BacktrackSolution(),
-		"bruteforce": BruteforceSolution()
+		"Pysat": PysatSolution(),
+		"Backtrack": BacktrackSolution(),
+		"Bruteforce": BruteforceSolution()
 	}
 
 	for name, sol in solutions.items():
@@ -66,12 +73,11 @@ def run(file_name):
 
 def main():
 	input_files = [
-		"../asset/input_5.txt",
-		"./asset/input_4.txt",
-		"../asset/input_2.txt",
-		"../asset/input_3.txt",
-		"../asset/input_1.txt",
-		"../asset/input_6.txt",
+		# "../asset/input_5.txt",
+		# "./asset/input_4.txt",
+		# "../asset/input_2.txt",
+		# "../asset/input_1.txt",
+		"../asset2/input_4.txt",
 	]
 	for file in input_files:
 		run(file)
